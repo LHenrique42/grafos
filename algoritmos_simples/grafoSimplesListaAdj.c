49c38d24a89196b65b94032d01e02e6c54916996
Luis Henrique <henriquegundes@hotmail.com>
2018-09-22 15:42:56 -0300

    First Commit - Apenas uns códigos bagunçados

---
+++ algoritmos_simples/grafoSimplesListaAdj.c
@@ -0,0 +1,194 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+TODO: Label do vertice nao esta funcionando
+*/
+typedef struct vertice
+{
+    int no;
+    struct vertice *prox;
+} Vertice;
+
+typedef struct grafo
+{
+    int V;
+    int E;
+    Vertice *adj;
+} Grafo;
+
+Grafo *criaGrafo(int v);
+Vertice *novoVertice(int x);
+void insereAresta(Grafo *G, int v, int w);
+void removeAresta(Grafo *G, int v, int w);
+void imprimeGrafo(Grafo *G);
+int grauVertice(Grafo *G, int v);
+int GrafosGrausVerticesIguais(Grafo *A, Grafo *B);
+
+
+
+
+/*
+Tarefas:
+- Imprimir a estrutura de armazenamento do grafo;
+- Calcular o grau de um vértice;
+- Verificar se a sequência de graus dos vértices de dois grafos são
+iguais.
+- Função main para testar todas as outras funções.
+*/
+
+void main()
+{
+    Grafo *G = criaGrafo(4);
+    insereAresta(G, 0, 1);
+	insereAresta(G, 0, 2);
+	insereAresta(G, 0, 3);
+	insereAresta(G, 1, 2);
+	insereAresta(G, 1, 3);
+	insereAresta(G, 2, 1);
+	insereAresta(G, 2, 3);
+	removeAresta(G, 2, 3);
+    imprimeGrafo(G);
+    printf("%2d\n", grauVertice(G, 0));
+
+    Grafo *A = criaGrafo(4);
+    insereAresta(A, 0, 1);
+	insereAresta(A, 0, 2);
+	insereAresta(A, 0, 3);
+	insereAresta(A, 1, 2);
+	insereAresta(A, 1, 3);
+	insereAresta(A, 2, 1);
+	insereAresta(A, 2, 3);
+//	removeAresta(A, 2, 3);
+
+    printf("%2d\n", GrafosGrausVerticesIguais(G, A));
+}
+
+Grafo *criaGrafo(int v)
+{
+    Grafo *G = (Grafo  *)malloc(sizeof(Grafo));
+    G->V = v;
+    G->E = 0;
+    G->adj = (Vertice *)calloc(v, sizeof(Vertice));
+    return G;
+}
+
+Vertice *novoVertice(int x)
+{
+    Vertice *novo = (Vertice *)malloc(sizeof(Vertice));
+    if(novo == NULL ) {printf("ERRO!\n"); exit(1);}
+    novo->no = x;
+    novo->prox = NULL;
+    return novo;
+}
+
+void insereAresta(Grafo *G, int v, int w)
+{
+    if( v != w )
+    {
+        Vertice *p = G->adj[v].prox;
+        while( p != NULL )
+        {
+            if( p->no == w ) break;
+            p = p->prox;
+        }
+        if( p == NULL ) // nao existe aresta (v,w)
+        {
+            Vertice *novo = novoVertice(w);
+            novo -> prox = G->adj[v].prox;
+            G->adj[v].prox = novo;
+            G->E++;
+        }
+        p = G->adj[w].prox;
+        while( p != NULL )
+        {
+            if( p -> no == v ) break;
+            p = p->prox;
+        }
+        if(p == NULL) // nao existe aresta (w,v)
+        {
+            Vertice *novo = novoVertice(v);
+            novo -> prox = G->adj[w].prox;
+            G->adj[w].prox = novo;
+            G->E++;           
+        }
+    }
+}
+
+void removeAresta(Grafo *G, int v, int w)
+{
+    if(G->adj[v].prox != NULL)
+    {
+        Vertice *aux = G->adj[v].prox;
+        Vertice *preaux = &(G->adj[v]);
+        while(aux != NULL)
+        {
+            if(aux -> no == w) break;
+            preaux = aux;
+            aux = aux->prox;
+        }
+        if(aux != NULL)
+        {
+            G->E--;
+            preaux -> prox = aux->prox;
+            free(aux);
+        }
+    }
+    if(G->adj[w].prox != NULL)
+    {
+        Vertice *aux = G->adj[w].prox;
+        Vertice *preaux = &(G->adj[w]);
+        while(aux != NULL)
+        {
+            if(aux -> no == v) break;
+            preaux = aux;
+            aux = aux->prox;
+        }
+        if(aux != NULL)
+        {
+            G->E--;
+            preaux -> prox = aux->prox;
+            free(aux);
+        }        
+    }
+}
+
+void imprimeGrafo(Grafo *G)
+{
+    if(G ==  NULL) exit(1);
+    Vertice *aux = NULL;
+    for(int i = 0; i < G->V; i++)
+    {
+        aux = G->adj[i].prox;
+        printf("%2d:", i);
+        while( aux != NULL )
+        {
+            printf(" %2d", aux->no);
+            aux = aux -> prox;
+        }
+        puts("");
+    }
+}
+
+int grauVertice(Grafo *G, int v)
+{
+    if(G == NULL) return 0;
+    Vertice *aux = G->adj[v].prox;
+    int grau = 0;
+    while(aux != NULL)
+    {
+        aux = aux -> prox;
+        grau++;
+    }
+    return grau;
+}
+
+int GrafosGrausVerticesIguais(Grafo *A, Grafo *B)
+{
+    if(A->V != B->V) return 0;
+    for( int i = 0; i < A->V; i++)
+    {
+        if( grauVertice(A, i) != grauVertice(B, i)) return 0;
+    }
+    return 1;
+}
---
+++ algoritmos_simples/grafoSimples_MatrizAdj.c
@@ -0,0 +1,138 @@
+/*
+	As seguintes implementações não é para grafos
+	valorados, direcionados. Apenas grafos simples, implementados
+	utilizanto matriz de adjacencias.
+
+*/
+
+#include <stdlib.h>
+#include <stdio.h>
+
+// Estrutura para Grafo
+typedef struct grafo
+{
+	int V;
+	int E;
+	int **adj;
+	
+} Grafo;
+
+// Protótipos de funções
+
+Grafo *criaGrafo(int );
+int **alocaMatrizInt(int, int, int);
+void insereAresta(Grafo *, int , int);
+void removeAresta(Grafo *, int, int);
+void imprimeGrafo(Grafo *);
+int verificaAdjacencia(Grafo *, int, int);
+int grauVertice(Grafo *, int);
+void imprimeAdjacentes(Grafo *, int);
+
+/*
+Tarefas:
+- Implemente uma função que recebe um grafo e dois vértices, x e y,
+retorna 1 caso o vértice x e y são adjacentes e 0 caso contrário.
+- Implemente uma função que calcula o grau de um vértice.
+- Implemente uma função que recebe um grafo e um vértice x, e
+imprime todos os vértices adjacentes a x.
+*/
+
+// Fim Protótipos
+
+void main(int argc, int **argv)
+{
+	Grafo *G = criaGrafo(4);
+	insereAresta(G, 0, 1);
+	insereAresta(G, 0, 2);
+	insereAresta(G, 0, 3);
+	insereAresta(G, 1, 2);
+	insereAresta(G, 1, 3);
+	insereAresta(G, 2, 1);
+	insereAresta(G, 2, 3);
+	removeAresta(G, 2, 3);
+	imprimeGrafo(G);
+	printf("%2d\n", verificaAdjacencia(G, 2, 3));
+	printf("%2d\n", verificaAdjacencia(G, 0, 1));
+	printf("%2d\n", grauVertice(G, 0));
+	imprimeAdjacentes(G, 0);
+	puts("");
+}
+
+int **alocaMatrizInt(int m, int n, int valorInicial)
+{
+	int **matriz = (int **)malloc(m * sizeof(int *));
+	for(int i = 0; i < n; i++) matriz[i] = (int *)malloc(n * sizeof(int ));
+	for(int i = 0; i < m; i++)
+	{
+		for(int j = 0; j < n; j++) matriz[i][j] = valorInicial;
+	}
+	return matriz;
+}
+
+Grafo *criaGrafo(int v)
+{
+	Grafo *G = (Grafo *)malloc(1 * sizeof(Grafo));
+	G->V = v;
+	G->E = 0;
+	G->adj = alocaMatrizInt(v, v, 0);
+	return G;
+}
+
+void insereAresta(Grafo *G, int v, int w)
+{
+	if(v != w && G->adj[v][w] == 0)
+	{
+		G->adj[v][w] = 1;
+		G->adj[w][v] = 1;
+		G->E++;
+	}
+}
+
+void removeAresta(Grafo *G, int v, int w)
+{
+	if(G->adj[v][w] == 1)
+	{
+		G->adj[v][w] = 0;
+		G->adj[w][v] = 0;
+		G->E--;
+	}
+}
+
+void imprimeGrafo(Grafo *G)
+{
+	for(int v = 0; v < G->V; v++)
+	{
+		printf("%2d:", v);
+		for(int w = 0; w < G->V; w++)
+		{
+			if(G->adj[v][w] == 1)
+				printf(" %2d", w);
+		}
+		puts("");
+	}
+}
+
+int verificaAdjacencia(Grafo *G, int v, int w)
+{
+	if(G == NULL) return 0;
+	if(G->adj[v][w] == 1 && G->adj[w][v] == 1)
+		return 1;
+	return 0;
+}
+
+int grauVertice(Grafo *G, int v)
+{
+	if(G == NULL) return 0;
+	int grau = 0;
+	for(int i = 0; i < G->V; i++) grau += G->adj[v][i];
+	return grau;
+}
+
+void imprimeAdjacentes(Grafo *G, int v)
+{
+	printf("%2d:", v);
+	for(int i = 0; i < G->V; i++)
+	{
+		if(G->adj[v][i] == 1) printf(" %2d", i);
+	}
+}
